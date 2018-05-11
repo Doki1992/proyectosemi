d31478eef32a2d79f7e350f79247a4b7b6850b66
mike <mike58072@gmail.com>
2018-05-11 01:41:51 -0600

    add1

--- views/site/index.php
+++ views/site/index.php
@@ -9,7 +9,7 @@ $this->title = 'Portafolio ';
     <div class="jumbotron">
         <h1>Bienvenido</h1>
 
-        <p class="lead">Desarrollador de paginas web</p>
+        <p class="lead">Desarrollador de paginas web 1</p>
 
         <!--<p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a>            
         </p>-->
